25086a07d368fa1e51bab12d3115da5dce8aee5d
Samara Albertos <salbbla@canariaseducacion.es>
2026-02-24 12:46:35 +0000

    Añadimos los check para la limpieza de datos

--- test_checks.py
+++ test_checks.py
@@ -15,3 +15,35 @@ def check_no_vacio(leer_datos: pd.DataFrame):
             "explicacion": MetadataValue.text("El dataset no puede estar vacío."),
         },
     )
+
+
+@asset_check(asset="limpiar_datos")
+def check_normalizacion_datos(limpiar_datos: pd.DataFrame):
+    datos_territorio = (limpiar_datos["territorio"] == limpiar_datos["territorio"].str.strip().str.lower()).all()
+    datos_medida = (limpiar_datos["medida"] == limpiar_datos["medida"].str.strip().str.lower()).all()
+
+    passed = bool(datos_territorio and datos_medida)
+
+    return AssetCheckResult(
+        passed=passed,
+        metadata={
+            "territorio_normalizado": MetadataValue.text(str(datos_territorio)),
+            "medida_normalizada": MetadataValue.text(str(datos_medida)),
+            "explicacion": MetadataValue.text(
+                "Normalizamos categorías y evitamos duplicados"
+            ),
+        },
+    )
+
+@asset_check(asset="renta_2022")
+def check_anio_2022(renta_2022: pd.DataFrame):
+    anios = sorted(renta_2022["anio"].unique().tolist()) if "anio" in renta_2022.columns else []
+    passed = (anios == [2022])
+
+    return AssetCheckResult(
+        passed=passed,
+        metadata={
+            "anios_detectados": MetadataValue.text(str(anios)),
+            "explicacion": MetadataValue.text("Filtramos por el año 2022."),
+        },
+    )
